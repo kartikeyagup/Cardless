2424d1714a584dc6c84d6d4ad2a063d54415d81e
kartikeyagup <kartikeyagupta@hotmail.com>
2015-06-08 21:10:36 +0530

    Basic working code added in windows 8.1

--- Cardless/Cardless/Cardless.Windows/MainPage.xaml.cs
+++ Cardless/Cardless/Cardless.Windows/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel;
+using Windows.Graphics.Imaging;
+using WindowsPreview.Media.Ocr;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +25,127 @@ namespace Cardless
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        OcrEngine ocrEngine;
+        UInt32 width;
+        UInt32 height;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            ocrEngine = new OcrEngine(OcrLanguage.English);
         }
+
+        public bool CheckEmail(string x)
+        {
+            bool sofar = false;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == '@')
+                {
+                    sofar = true;
+                }
+            }
+            return sofar;
+        }
+
+        public bool CheckNumber(string x)
+        {
+            List<char> allowedchar = new List<char>(new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '(', ')' });
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (allowedchar.IndexOf(x[i]) < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public List<string> GetEmailIds(List<string> inpwords)
+        {
+            List<string> anslist = new List<string>();
+            foreach (string word in inpwords)
+            {
+                if (CheckEmail(word))
+                {
+                    anslist.Add(word);
+                }
+            }
+            return anslist;
+        }
+
+        public List<string> GetPhoneNumbers(List<string> inpwords)
+        {
+            List<string> anslist = new List<string>();
+            foreach (string word in inpwords)
+            {
+                if (CheckNumber(word))
+                {
+                    anslist.Add(word);
+                }
+            }
+            return anslist;
+        }
+
+        public string JoinList(List<string> inp)
+        {
+            string ans = "";
+            foreach (string elem in inp)
+            {
+                ans += elem + ",";
+            }
+            return ans;
+        }
+
+        protected async override void OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            // Load image from install folder.
+            //ArrayList allwords = new ArrayList();
+            var file = await Package.Current.InstalledLocation.GetFileAsync("shreydad.png");
+            List<string> wordlist1 = new List<string>();
+            using (var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                // Create image decoder.
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+
+                width = decoder.PixelWidth;
+                height = decoder.PixelHeight;
+
+                // Get pixels in BGRA format.
+                var pixels = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Straight,
+                    new BitmapTransform(),
+                    ExifOrientationMode.RespectExifOrientation,
+                    ColorManagementMode.ColorManageToSRgb);
+
+                // Extract text from image.
+                OcrResult result = await ocrEngine.RecognizeAsync(height, width, pixels.DetachPixelData());
+
+                // Check whether text is detected.
+                if (result.Lines != null)
+                {
+                    // Collect recognized text.
+                    string recognizedText = "";
+                    foreach (var line in result.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            //allwords.Add(word.Text);
+                            wordlist1.Add(word.Text);
+                            recognizedText += word.Text + " ";
+                        }
+                        recognizedText += Environment.NewLine;
+                    }
+
+                    // Display recognized text.
+                    OcrText.Text = recognizedText;
+                    EmailText.Text = JoinList(GetEmailIds(wordlist1)) + "\n" + JoinList(GetPhoneNumbers(wordlist1));
+                }
+            }
+        }
+
     }
 }
