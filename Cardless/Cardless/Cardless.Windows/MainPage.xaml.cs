542c61a5301478eddc0cba42ca9484e4e807ab5f
pandeyshrey33 <pandeyshrey33@gmail.com>
2015-06-23 23:53:56 +0530

    My first commit.

--- Cardless/Cardless/Cardless.Windows/MainPage.xaml.cs
+++ Cardless/Cardless/Cardless.Windows/MainPage.xaml.cs
@@ -12,9 +12,25 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Streams;
 using Windows.ApplicationModel;
 using Windows.Graphics.Imaging;
 using WindowsPreview.Media.Ocr;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Media.Capture;
+
+
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,17 +39,20 @@ namespace Cardless
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// public static MainPage Current
     public sealed partial class MainPage : Page
     {
-
+        public static MainPage Current;
         OcrEngine ocrEngine;
         UInt32 width;
+        
+        MainPage rootPage = MainPage.Current;
         UInt32 height;
 
         public MainPage()
         {
             this.InitializeComponent();
-
+            Current = this;
             ocrEngine = new OcrEngine(OcrLanguage.English);
         }
 
@@ -144,6 +163,42 @@ namespace Cardless
                     OcrText.Text = recognizedText;
                     EmailText.Text = JoinList(GetEmailIds(wordlist1)) + "\n" + JoinList(GetPhoneNumbers(wordlist1));
                 }
+            }
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                //rootPage.NotifyUser("", NotifyType.StatusMessage);
+
+                // Using Windows.Media.Capture.CameraCaptureUI API to capture a photo
+                CameraCaptureUI dialog = new CameraCaptureUI();
+                Size aspectRatio = new Size(16, 9);
+                dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+                StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (file != null)
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                    {
+                        bitmapImage.SetSource(fileStream);
+                    }
+                    CapturedPhoto.Source = bitmapImage;
+                    //ResetButton.Visibility = Visibility.Visible;
+
+                    // Store the file path in Application Data
+                    //appSettings[photoKey] = file.Path;
+                }
+                else
+                {
+                    //rootPage.NotifyUser("No photo captured.", NotifyType.StatusMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                //rootPage.NotifyUser(ex.Message, NotifyType.ErrorMessage);
             }
         }
 
